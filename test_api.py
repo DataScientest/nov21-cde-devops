a11b4b3cb7b0a6636257c2242481e0c42c3a8c43
pauldechorgnat <pdechorgnat@yahoo.fr>
2022-03-31 13:20:55 +0200

    ajout des fichiers de tests

---
+++ test_api.py
@@ -0,0 +1,52 @@
+import requests
+
+API_URL = "http://127.0.0.1:8000"
+
+
+def test_get_index():
+    response = requests.get(
+        url=f"{API_URL}/"
+    )
+
+    assert response.status_code == 200, response.content
+
+
+def test_get_health():
+    response = requests.get(
+        url=f"{API_URL}/health"
+    )
+
+    assert response.status_code == 200, response.content
+    assert response.json()["status"] == 1
+
+
+def test_post_health():
+    response = requests.post(
+        url=f"{API_URL}/health"
+    )
+
+    assert response.status_code == 200, response.content
+    assert response.json()["status"] == 1
+    assert "comment" in response.json()
+
+
+def test_get_object():
+
+    object_id = 123
+
+    response = requests.get(
+        url=f"{API_URL}/objects",
+        params={"object_id": object_id}
+    )
+
+    assert response.status_code == 200, response.content
+    assert response.json()["object_id"] == object_id
+
+    object_id = 1234
+
+    response = requests.get(
+        url=f"{API_URL}/objects",
+        params={"object_id": object_id}
+    )
+
+    assert response.status_code == 404, response.content
